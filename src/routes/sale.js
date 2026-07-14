29c38cfcce7210b811782d1400afce6c486a61bf
Samy-Garcia <20240528@ricaldone.edu.sv>
2026-07-14 14:22:40 -0600

    Actualizar sale.js

---
+++ src/routes/sale.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import saleController from "../controllers/saleController.js";
+
+import { validateAuthCookie } from "../middlewares/validateAuthCookie.js";
+
+const router = Router();
+
+// Cliente y Admin pueden consultar una compra por ID
+router.get(
+    "/:id",
+    validateAuthCookie(["customer", "admin"]),
+    saleController.getSale
+);
+
+// Solo Admin puede consultar todas las compras
+router.get(
+    "/",
+    validateAuthCookie(["admin"]),
+    saleController.getSales
+);
+
+// Solo Cliente puede registrar una compra
+router.post(
+    "/",
+    validateAuthCookie(["customer"]),
+    saleController.createSale
+);
+
+// Admin puede actualizar una compra
+router.put(
+    "/:id",
+    validateAuthCookie(["admin"]),
+    saleController.updateSale
+);
+
+// Solo Admin puede eliminar una compra
+router.delete(
+    "/:id",
+    validateAuthCookie(["admin"]),
+    saleController.deleteSale
+);
+
+export default router;
